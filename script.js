cda09edbc58a6dcbefc497c71b19d78b9269b330
Gonzalo Rando <randosgonzalo@gmail.com>
2021-11-02 17:25:46 -0300

    Desafío 1 - Crear un algoritmo JS simple

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombreUsuario = prompt ("Ingrese su nombre");
+let apellidoUsuario = prompt ("Ingrese su apellido");
+const espacioBlanco = " ";
+let alertaBienvenida = "!Bienvenido,";
+let alertaBienvenidaDos = "¡"
+let datosUsuario = alertaBienvenida + espacioBlanco + nombreUsuario + espacioBlanco + apellidoUsuario + alertaBienvenidaDos;
+alert(datosUsuario);
